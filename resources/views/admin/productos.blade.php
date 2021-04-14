396ac42e6f4b9850c8dcb2bbb92f46e8df783e69
TinoCH8 <71781767+TinoCH8@users.noreply.github.com>
2021-04-14 11:20:44 -0600

    Sistema de plantillas de Blade

---
+++ resources/views/admin/productos.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.main')
+
+@section('contenido')
+    <h2>Estoy en el los productos</h2>
+
+@endsection
+
+@section('scripts')
+<script> console.log("asdsad"); </script>
+@endsection
---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.main')
+
+@section('contenido')
+    <h2>Estoy en el admin</h2>
+
+@endsection
+
+@section('scripts')
+<script> console.log("asdsad"); </script>
+@endsection
